052640c922cb90fc226119fcccb7800db6eb2cce
xipuhu <542634344@qq.com>
2019-03-14 15:49:51 +0800

    code with leetcode

--- Top 100 Liked Questions/easy/1.cpp
+++ Top 100 Liked Questions/easy/1.cpp
@@ -1,4 +1,4 @@
-原题内容：
+//原题内容：
 
 1. Two Sum
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
@@ -12,7 +12,7 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 
 
-第一遍解法代码：
+//第一遍解法代码：
 //时间复杂度O(n^2) 144ms beats 35.96%
 vector<int> twoSum(vector<int>& nums, int target) {
     vector<int> indices;
@@ -32,7 +32,7 @@ vector<int> twoSum(vector<int>& nums, int target) {
     return indices;
 }
 
-网上好的解法：
+//网上好的解法：
 //方法一：使用一个哈希表，存储每个数对应的下标
 时间复杂度：O(n) 12ms beats 97.81%
 vector<int> twoSum(vector<int> &nums, int target) {
@@ -55,7 +55,7 @@ vector<int> twoSum(vector<int> &nums, int target) {
     return result;
 }
 
-精简优化：
+//精简优化：
 12ms beats 97.81%
 vector<int> twoSum(vector<int>& nums, int target) {
     unordered_map<int, int> hash_table;
@@ -87,7 +87,7 @@ vector<int> twoSum(vector<int>& nums, int target) {
     }
     return vector<int>();
 }
-
+/*
 思考：
 1. 解法思路方面：
    本题有两个技巧是需要注意的：
@@ -106,5 +106,5 @@ vector<int> twoSum(vector<int>& nums, int target) {
 Map和unordered_map的区别：map内部实现了一个红黑树，该结构具有自动排序的功能，因此map内部的所有元素都是有序的，红黑树的每一个节点都代表着map的一个元素，因此，对于map进行的查找，删除，添加等一系列的操作都相当于是对红黑树进行这样的操作，故红黑树的效率决定了map的效率; 而unordered_map: unordered_map内部实现了一个哈希表，因此其元素的排列顺序是杂乱的，无序的。在查找的时候，map的效率要比unordered_map低很多。
 
 在map中，由key查找value时，首先要判断map中是否包含key。如果不检查，直接返回map[key]，可能会出现意想不到的行为。如果map包含key，没有问题，如果map不包含key，使用下标有一个危险的副作用，会在map中插入一个key的元素，value取默认值，返回value。也就是说，map[key]不可能返回null。map提供了两种方式，查看是否包含key，m.count(key)，m.find(key)。
-
+*/
 
