81bc75d329b77566a98bb0d4e7e656d0ea6c3bbe
xipuhu <542634344@qq.com>
2019-03-26 23:56:10 +0800

    Update 206.cpp

--- Top 100 Liked Questions/easy/206.cpp
+++ Top 100 Liked Questions/easy/206.cpp
@@ -1,4 +1,4 @@
-/* ԭ������
+/* 原题内容
 206. Reverse Linked List
 
 Reverse a singly linked list.
@@ -15,7 +15,7 @@ A linked list can be reversed either iteratively or recursively. Could you imple
 */
 
 
-// ��һ��ⷨ����
+// 第一遍解法代码
 /* info:
 *Runtime: 8 ms, faster than 100.00%
 *Memory Usage: 9.1 MB, less than 74.07%
@@ -66,7 +66,7 @@ ListNode* reverseList(ListNode* head) {
 
     while (head != NULL){
         p1 = head;
-        head = head->next;  // ��ʱp1�Ϳ��Ժ�ʣ���list���뿪��
+        head = head->next;  // 此时p1就可以和剩余的list脱离开了
 
         p1->next = p2;
         p2 = p1;
@@ -77,12 +77,12 @@ ListNode* reverseList(ListNode* head) {
 
 }
 
-// ���ϺõĽⷨ
+// 网上好的解法
 /* info:
 *Runtime: 8 ms, faster than 100.00%
 *Memory Usage: 9.1 MB, less than 85.68%
 */
-// ����
+// 迭代
 ListNode* reverseList(ListNode* head) {
         ListNode* prev = NULL;
         ListNode* curr = head;
@@ -99,7 +99,7 @@ ListNode* reverseList(ListNode* head) {
 *Runtime: 8 ms, faster than 100.00%
 *Memory Usage: 9.2 MB, less than 28.52%
 */
-// �ݹ�
+// 递归
 ListNode* reverseList(ListNode* head) {
         if (!head || !(head -> next)) {
             return head;
@@ -110,7 +110,7 @@ ListNode* reverseList(ListNode* head) {
         return node;
 }
 
-// �����Ż�
+// 精简优化
 /* info:
 *Runtime: 12 ms, faster than 100.00%
 *Memory Usage: 10 MB, less than 68.28%
@@ -118,7 +118,7 @@ ListNode* reverseList(ListNode* head) {
 ListNode* reverseList(ListNode* head) {
     if (head == NULL || head->next == NULL) return head;
 
-    ListNode *p = reverseList(head->next);  // ��ִ����һ���󣬾Ͳ��������head�Ĳ�����Ӱ��ʣ��list��
+    ListNode *p = reverseList(head->next);  // 先执行这一步后，下面对head的操作会影响剩余list了
 
     head->next->next = head;
     head->next = NULL;
@@ -127,16 +127,16 @@ ListNode* reverseList(ListNode* head) {
 
 }
 
-/* ˼��
-1. �ⷨ˼·���棺
+/* 思考
+1. 解法思路方面：
 
-�ڵ���list�Ĺ����У����ú÷��뵱ǰ����ʣ��list�ļ��ɣ��Ϳ��Ժ����׵�ʵ��list��ת
+在迭代list的过程中，利用好分离当前结点和剩余list的技巧，就可以很容易的实现list反转
 
-�ݹ�˼�룺��ǰ���Ϊn_k������list��ʣ�ಿ�֣�n_k+1�������Ѿ��Ƿ�ת�õ�list�������ʹ��n_k->next->next = n_k����ʵ��n_k+1 -> next = n_k;
+递归思想：当前结点为n_k，假设list的剩余部分（n_k+1…）都已经是反转好的list，则可以使用n_k->next->next = n_k，来实现n_k+1 -> next = n_k;
 
-2. ���������棺
+2. 代码解决方面：
 
-���ڵ�������list�еĽ���ʱ��һ��Ҫ����ȫ���ý���list�з��������ע�⣬�ڵ�һ��ⷨ�����У�ʹ��stack������洢����ʱ�����û�н������ȫ��������Ļ�����ôlist����û�䣬����ķſ���ʹ��node->next = NULL��ʵ�֡�
+对于单独处理list中的结点的时候，一定要先完全将该结点从list中分离出来。注意，在第一遍解法代码中，使用stack来另外存储结点的时候，如果没有将结点完全分离从来的话，那么list依旧没变，分离的放可以使用node->next = NULL来实现。
 
 */
 
